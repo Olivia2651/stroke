083ec75dd68193cac96324d0702b1e45ae76a37d
Olivia2651 <98221158+Olivia2651@users.noreply.github.com>
2023-03-03 14:06:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,10 @@ def main():
   hypertension = st.slider("Input if you have hypertension, 0 if no and 1 if yes",0,1)
   heartdisease = st.slider("Input if you have heart disease, 0 if no and 1 if yes",0,1)
   ever_married=st.slider('Input your marriage status,0 if no and 1 if yes',0,1)
-  work_type= st.select_slider('Input the type of work you do ',options=[2,3,0,4,1])
-  st.write("Selected value:", work_type)
-  sugar=st.slider("Input your average glucose level",50.0,300.000)
-  bmi=st.slider("Input your BMI",0.0,70.0)
-  smoking_status=st.select_slider('Input your smoking status ',options=[0,1,2,3])
+  work_type= st.select_slider('Input the type of work you do ',options=[2,3,0,4,1],label_visibility="visible")
+  sugar=st.number_input("Input your average glucose level", min_value=None, max_value=None)
+  bmi=st.number_input("Input your BMI", min_value=None, max_value=None)
+  smoking_status=st.select_slider('Input your smoking status ',options=[0,1,2,3],label_visibility="visible")
   
 
   inputs=[[age,hypertension,heartdisease,ever_married,work_type,sugar,bmi,smoking_status]]
