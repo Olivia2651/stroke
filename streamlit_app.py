a2e651391fb9ec642ffeb195ac62e4c56fe0237c
Olivia2651 <98221158+Olivia2651@users.noreply.github.com>
2023-03-03 20:58:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,20 @@ model = pickle.load(open('LR2_pk.pkl','rb'))
 
 
 def main():
+  st.image("http://www.pngmart.com/files/3/Health-PNG-File.png")
   st.title("Stroke Risk Prediction")
   
   st.sidebar.header("This is a web app that tells you the predicted wether you will have a stroke or not.")
   
   st.subheader("Just fill in the information")
   age = st.slider("Input your age.",0,100)
-  hypertension = st.slider("Input if you have hypertension.\n \n 0 if no and 1 if yes",0,1)
-  heartdisease = st.slider("Input if you have heart disease.\n \n 0 if no and 1 if yes",0,1)
-  ever_married=st.slider('Input your marriage status.\n \n0 if no and 1 if yes',0,1)
-  work_type= st.selectbox('Input the type of work you do.\n2 \n if Private ,3 if Self-employed ,0 if Govt_job ,4 if children ,1 if Never_worked',(2,3,0,4,1))
+  hypertension = st.slider("Input if you have hypertension. 0 if no and 1 if yes",0,1)
+  heartdisease = st.slider("Input if you have heart disease.0 if no and 1 if yes",0,1)
+  ever_married=st.slider('Input your marriage status.0 if no and 1 if yes',0,1)
+  work_type= st.selectbox('Input the type of work you do.2 if Private , 3 if Self-employed , 0 if Govt_job , 4 if children , 1 if Never_worked',(2,3,0,4,1))
   sugar=st.number_input("Input your average glucose level", min_value=None, max_value=None)
   bmi=st.number_input("Input your BMI", min_value=None, max_value=None)
-  smoking_status=st.selectbox('Input your smoking status.\n \n 1 if formerly smoked ,2 if never smoked ,3 if smokes ,0 if Unknown' ,(1,2,3,0))
+  smoking_status=st.selectbox('Input your smoking status.1 if formerly smoked , 2 if never smoked , 3 if smokes , 0 if Unknown' ,(1,2,3,0))
   
 
   inputs=[[age,hypertension,heartdisease,ever_married,work_type,sugar,bmi,smoking_status]]
